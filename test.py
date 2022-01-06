d2d6d84b5f34c03cb4179c3b562c3adfa6bbcb8b
Kellen Cheng <54748182+ktcheng@users.noreply.github.com>
2022-01-06 15:21:11 -0800

    Adding test files

---
+++ test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  6 15:11:56 2022
+
+@author: Kellen Cheng
+"""
+
+if __name__ == "__main__":
+    x = "ECE_180_DA_DB"
+    if x == "EE_180_DA_DB":
+        print("You are living in 2017")
+    else:
+        x = x + " - Best class ever"
+        print(x)
